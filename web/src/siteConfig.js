25327aa940eb969300a4d9cdc6441700be6e3707
jkljklkjj <2998568539@qq.com>
2026-05-02 09:24:51 +0800

    更新下载地址

--- web/src/siteConfig.js
+++ web/src/siteConfig.js
@@ -1,12 +1,13 @@
 // 全站可配置项（简单单文件配置）
 const siteConfig = {
-	ICP_NUMBER: '粤ICP备2025156789号',
+	ICP_NUMBER: '粤ICP备2026049055号',
 	RELEASE_BASE: 'https://github.com/jkljklkjj/CharRoom/releases/latest',
+	RELEASE_VERSION: 'build-25239911453',
 	INSTALLER_URLS: {
-		android: 'https://kkgithub.com/jkljklkjj/CharRoom/releases/download/build-25220511671/chatlite.apk',
-		linux: 'https://kkgithub.com/jkljklkjj/CharRoom/releases/download/build-25220511671/chatlite.deb',
-		macos: 'https://kkgithub.com/jkljklkjj/CharRoom/releases/download/build-25220511671/chatlite.dmg',
-		windows: 'https://kkgithub.com/jkljklkjj/CharRoom/releases/download/build-25220511671/chatlite.msi'
+		android: 'https://kkgithub.com/jkljklkjj/CharRoom/releases/download/{RELEASE_VERSION}/chatlite.apk',
+		linux: 'https://kkgithub.com/jkljklkjj/CharRoom/releases/download/{RELEASE_VERSION}/chatlite.deb',
+		macos: 'https://kkgithub.com/jkljklkjj/CharRoom/releases/download/{RELEASE_VERSION}/chatlite.dmg',
+		windows: 'https://kkgithub.com/jkljklkjj/CharRoom/releases/download/{RELEASE_VERSION}/chatlite.msi'
 	},
 	// 可配置项：作者邮箱与支付二维码图片路径
 	AUTHOR_EMAIL: '2998568539@qq.com',
